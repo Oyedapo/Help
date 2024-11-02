431967d1adacff12247e524c2fcaa55b4457f8b3
Monisola <bellomonisola867@gmail.com>
2024-11-02 19:16:45 +0100

    added the ids and a format you could follow

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-// const products = [
-//   {
-//     id: "Shortpasta",
-//      name : "Pasta",
-//     image: 'images\Baked feta pasta.jpg',
-//     keyword : "grain"
-// }
-// ]
+const products = [
+  {
+    id: "72j4-k8-q9p5",
+    name: "Feta Pasta",
+    image: "images/Baked feta pasta.jpg",
+    keyword: ["feta", "penne"],
+    priceCents:  1000
+  }
+]
+
+//the price is in cents
+
+/*
+here's all of the ids i generated for each product
+please do  not mix them up else, we'd have countless errors
+just appoint one to each product
+
+1. 72j4-k8-q9p5
+2. p2m5-x1-g9w0
+3. g9w0-d4-72j4
+4. k8q9-p5-72j4
+5. p572-j4-k8q9
+6. 9w0d-47-2j4k
+7. 472j-4k-8q9p
+8. j4k8-q9-p572
+9. k8q9-p5-72j4
+10. p572-j4-k8q9
+11. 9w0d-47-2j4k
+12. 472j-4k-8q9p
+*/
