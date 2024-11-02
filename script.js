69b84e2c588bd71b319b55655ba96d240bddef9d
Oyewole Oyedapo <oyewoleoyedapo2@gmail.com>
2024-11-02 22:27:39 +0100

    Version 1 updated

--- script.js
+++ script.js
@@ -1,8 +1,95 @@
-// const products = [
-//   {
-//     id: "Shortpasta",
-//      name : "Pasta",
-//     image: 'images\Baked feta pasta.jpg',
-//     keyword : "grain"
-// }
-// ]
+const products = [
+  {
+    id: "72j4-k8-q9p5",
+    name: "Tonkotsu Ramen",
+    image: "images/ramen.jpg",
+    keyword: ["ramen", "tonkotsu"],
+    priceCents:  1000
+  },
+  {
+    id: "p2m5-x1-g9w0",
+    name: "Sushi",
+    image: "images/sushi.jpg",
+    keyword: ["sushi"],
+    priceCents:  2500
+  },
+  {
+    id: "g9w0-d4-72j4",
+    name: "Spicy Karaage(Japanese fried chcicken)",
+    image: "images/spicy karaage.jpg",
+    keyword: ["chicken"],
+    priceCents:  1500
+  },
+  {
+    id: "k8q9-p5-72j4",
+    name: "Tempura",
+    image: "images/tempura.jpg",
+    keyword: ["tempura", "shrimp"],
+    priceCents:  1500
+  },
+  {
+    id: "p572-j4-k8q9",
+    name: "Okonomiyaki",
+    image: "images/Okonomiyaki.jpg",
+    keyword: ["okonomiyaki"],
+    priceCents:  1100
+  },
+  {
+    id: "1w0r-36-2d4h",
+    name: "Unagi Donburi (Grilled Eel Bowl)",
+    image: "images/Unari Donburi.jpg",
+    keyword: ["unagi"],
+    priceCents:  2500
+  },
+  {
+    id: "472j-4k-8q9p",
+    name: "Takoyaki",
+    image: "images/takoyaki.jpg",
+    keyword: ["takoyaki"],
+    priceCents:  900
+  },
+  {
+    id: "j4k8-q9-p572",
+    name: "Tonkatsu",
+    image: "images/tonkatsu.jpg",
+    keyword: ["tonkatsu"],
+    priceCents:  1400
+  },
+  {
+    id: "k2q9-p4-77j2",
+    name: "Udon Noodles",
+    image: "images/Udon.jpg",
+    keyword: ["Udon", "Noodles"],
+    priceCents:  1000
+  },
+  {
+    id: "p512-j3-k6q9",
+    name: "Sakura Matcha Dessert",
+    image: "images/Sakura Matcha Dessert.jpg",
+    keyword: ["dessert", "matcha"],
+    priceCents:  2500
+  },
+  {
+    id: "9w0d-47-2j4k",
+    name: "Yakitori (per skewer)",
+    image: "images/yakitori.jpg",
+    keyword: ["yakitori"],
+    priceCents:  400
+  },
+  {
+    id: "4p0w-4g-8w9w",
+    name: "Onigiri",
+    image: "images/Onigiri.jpg",
+    keyword: ["onigir", "rice"],
+    priceCents:  300
+  },
+  {
+    id: "4c0d-1g-3g7a",
+    name: "Sukiyaki",
+    image: "images/sukiyaki.jpg",
+    keyword: ["onigir", "rice"],
+    priceCents:  300
+  }
+]
+
+// I made it 13 sha 
